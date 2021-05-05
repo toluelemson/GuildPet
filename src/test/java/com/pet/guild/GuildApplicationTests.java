262708a3a736dd487ac818e6fc4fccb63f308d87
toluelemson <toluwanielemosho@gmail.com>
2021-05-05 15:09:18 +0300

    Bootstrap project

---
+++ src/test/java/com/pet/guild/GuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.guild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
