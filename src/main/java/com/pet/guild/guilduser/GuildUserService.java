06d8059d55bf72fc3a3fce0009bac03a341246be
toluelemson <toluwanielemosho@gmail.com>
2021-05-06 14:06:20 +0300

    sign-up user

--- src/main/java/com/pet/guild/guilduser/GuildUserService.java
+++ src/main/java/com/pet/guild/guilduser/GuildUserService.java
@@ -3,7 +3,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -12,7 +16,8 @@ public class GuildUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG =
             "Guild User with email %s not found";
 
-    private final GuildUserRepository guildUserRepository = null;
+    private final GuildUserRepository guildUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email)
@@ -23,4 +28,25 @@ public class GuildUserService implements UserDetailsService {
                                 String.format(USER_NOT_FOUND_MSG, email)));
     }
 
+    public String signUpUser(GuildUser guildUser) {
+
+        Boolean userExists = guildUserRepository
+                .findByEmail(guildUser.getEmail())
+                .isPresent();
+
+        if (userExists) {
+            // TODO check of attributes are the same and
+            // TODO if email not confirmed send confirmation email.
+
+            throw new IllegalStateException("email already taken");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder
+                .encode(guildUser.getPassword());
+
+        guildUser.setPassword(encodedPassword);
+        guildUserRepository.save(guildUser);
+
+        return "";
+    }
 }
