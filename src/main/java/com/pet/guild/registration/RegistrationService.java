023f1dfce6e8c77863954c8e902c791f5f9ce9f1
toluelemson <toluwanielemosho@gmail.com>
2021-05-06 14:07:18 +0300

    Registration service

--- src/main/java/com/pet/guild/registration/RegistrationService.java
+++ src/main/java/com/pet/guild/registration/RegistrationService.java
@@ -1,18 +1,34 @@
 package com.pet.guild.registration;
+import com.pet.guild.guilduser.GuildUser;
+import com.pet.guild.guilduser.GuildUserRole;
 import com.pet.guild.guilduser.GuildUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
 public class RegistrationService {
 
-    private final GuildUserService guildUserService;
+    private GuildUserService guildUserService;
+    private EmailValidator emailValidator;
 
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.
+                test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+
+        return guildUserService.signUpUser(
+                new GuildUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        GuildUserRole.USER
+
+                )
+        );
     }
 }
